2b46c8e3133236618dacbd452038c061e536cdf5
Carson <cpsievert1@gmail.com>
2020-09-03 10:55:07 -0500

    Revert back to using .form-group for container selector

--- inst/assets/shinyvalidate.js
+++ inst/assets/shinyvalidate.js
@@ -73,7 +73,7 @@ const bsStrategy = {
   },
   findInputContainer: function(el) {
     el = $(el);
-    const inputContainer = el.is(".shiny-input-container") ? el : el.parents(".shiny-input-container");
+    const inputContainer = el.is(".form-group") ? el : el.parents(".form-group");
     return inputContainer.length === 0 ? null : inputContainer;
   },
   setInvalid: function(el, binding, id, message) {
